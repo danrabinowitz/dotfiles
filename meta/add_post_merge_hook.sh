b97224c9afc82e3296d0af7e0c460603cc716e86
Dan Rabinowitz <djr@DanielRabinowitz.com>
2013-08-22 22:20:09 -0600

    WIP: Experimenting with post-merge hook

---
+++ meta/add_post_merge_hook.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "This is add_post_merge_hook.sh"
+echo "This is a placeholder. Soon it will actually copy the post_merge_hook.sh to .git/hooks"
+
+# Determine filename for post_merge_hook.sh
+
+# if filename does not already exist, copy the master to the filename
