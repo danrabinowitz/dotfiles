2a8d9cb41f795b835aa32e0d14515d0a6c258e69
Dan Rabinowitz <djr@DanielRabinowitz.com>
2013-10-07 08:49:43 -0600

    Updates to meta and gitignore

---
+++ meta/post-merge.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pwd=`pwd`
+echo "This is meta/post-merge.sh with 0=$0 and pwd=$pwd"
+. $pwd'/meta/process_dotfiles.sh'
--- meta/process_dotfiles.sh
+++ meta/process_dotfiles.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-
-echo 'This is process_dotfiles.sh'
+pwd=`pwd`
+echo "This is process_dotfiles.sh with pwd=$pwd"
 echo 'This is a placeholder. Soon this script will actually process dotfiles.'
+
+srcpath=$pwd'/src'
