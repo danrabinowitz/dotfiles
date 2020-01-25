5389b9200c2c0d499ee380cce2d7ced02d64d629
Dan Rabinowitz <djr@danielrabinowitz.com>
2020-01-24 17:44:21 -0700

    WIP: Emacs

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,3 +1,15 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
+
+
+
+
 (load "~/.emacs.d/lib/better-defaults/better-defaults.el")
 (load "~/.emacs.d/lib/typescript/typescript-mode.el")
-(load "~/.emacs.d/lib/tide/tide.el")
+(load "~/.emacs.d/lib/dash/dash.el")
+;(load "~/.emacs.d/lib/tide/tide.el")
+
+
