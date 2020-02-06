303426a59d02fb741549e189ddf27677f359300f
Dan Rabinowitz <djr@danielrabinowitz.com>
2020-02-06 06:21:47 -0700

    WIP: Cleanup

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -3,8 +3,6 @@
 (add-to-list 'load-path (concat user-emacs-directory "personal"))
 (load "package-config")
 
-
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
