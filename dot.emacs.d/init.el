9a253b74e5c8b9286b1f3a58a5b3860073c287c5
Dan Rabinowitz <djr@danielrabinowitz.com>
2020-02-05 20:56:56 -0700

    WIP: Emacs

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -1,22 +1,7 @@
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-(package-initialize)
+;;; This is the initial emacs lisp file, which is loaded by emacs on startup.
 
+(add-to-list 'load-path (concat user-emacs-directory "personal"))
+(load "package-config")
 
 (add-to-list 'load-path "~/.emacs.d/lib/use-package")
 (require 'use-package)
