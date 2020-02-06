9ec6d72b2120d6929a7a2a742eb6402c290b6d20
Dan Rabinowitz <djr@danielrabinowitz.com>
2020-02-06 07:26:15 -0700

    WIP: Emacs

--- dot.emacs.d/init.el
+++ dot.emacs.d/init.el
@@ -2,15 +2,13 @@
 
 (add-to-list 'load-path (concat user-emacs-directory "personal"))
 (load "package-config")
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (load "typescript-config")
 (load "golang-config")
+(load "nav-and-search-config")
+
+(global-set-key (kbd "C-c h b") 'describe-personal-keybindings)
 
 
 
