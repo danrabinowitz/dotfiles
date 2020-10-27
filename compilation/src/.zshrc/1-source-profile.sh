4d5c9eb04eb528a1d86c33aaf891a6691fbf4884
Dan Rabinowitz <djr@DanielRabinowitz.com>
2020-10-26 23:01:05 -0600

    WIP

---
+++ compilation/src/.zshrc/1-source-profile.sh
@@ -0,0 +1,11 @@
+description=<<'EOS'
+# Source ~/.profile if it exists
+EOS
+
+command='source'
+file='~/.profile'
+
+
+# command=<<'EOS'
+# [ -r ~/.profile ] && emulate sh -c 'source ~/.profile'
+# EOS
